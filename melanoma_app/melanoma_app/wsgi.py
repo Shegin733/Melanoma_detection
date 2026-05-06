6bac468d13abcd94b1eec058a612424f70aa3e84
Shegin733 <sheginismailap@gmail.com>
2026-05-06 14:36:19 +0530

    fix: correct django settings module path

--- melanoma_app/melanoma_app/wsgi.py
+++ melanoma_app/melanoma_app/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'melanoma_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'melanoma_app.melanoma_app.settings')
 
 application = get_wsgi_application()
