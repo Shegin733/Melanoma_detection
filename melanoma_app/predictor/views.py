35ef9a687cdef62286a9f5c84a282c529cf0d6c3
Shegin733 <sheginismailap@gmail.com>
2026-05-08 21:07:21 +0530

    'frontend-base'

--- melanoma_app/predictor/views.py
+++ melanoma_app/predictor/views.py
@@ -1,32 +1,36 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from .model import predict_image
 import os
 import uuid
 
 UPLOAD_DIR = "uploads"
+
 os.makedirs(UPLOAD_DIR, exist_ok=True)
-from django.http import HttpResponse
+
 
 def home(request):
-    return HttpResponse("Melanoma API is running")
-@csrf_exempt
+    return render(request, "index.html")
+
+
 def predict(request):
+
     if request.method == "POST":
+
         file = request.FILES.get("file")
 
         if not file:
-            return JsonResponse({"error": "No file"}, status=400)
+            return JsonResponse({
+                "error": "No file uploaded"
+            })
 
         filename = str(uuid.uuid4()) + ".jpg"
+
         path = os.path.join(UPLOAD_DIR, filename)
 
-        with open(path, "wb+") as f:
+        with open(path, "wb+") as destination:
             for chunk in file.chunks():
-                f.write(chunk)
+                destination.write(chunk)
 
         label, confidence = predict_image(path)
 
@@ -35,4 +39,6 @@ def predict(request):
             "confidence": round(confidence, 4)
         })
 
-    return JsonResponse({"message": "Send POST request"})
+    return JsonResponse({
+        "message": "Send POST request"
+    })
